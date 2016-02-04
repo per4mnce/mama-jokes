6d1da6f66e079117fdbec57c1a6c7ece16f3a17a
Henry Schaumburger <per4mnce@gmail.com>
2016-02-04 15:16:12 -0500

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -22,70 +22,74 @@
 'use strict';
 
 var AlexaSkill = require('./AlexaSkill'),
-    recipes = require('./recipes');
+    jokes = require('./data');
 
 var APP_ID = 'amzn1.echo-sdk-ams.app.2abbf8ec-05e3-49d1-b097-144663b180f7'; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
 
-var categories  = "fat, stupid, ugly, old, poor, short, skinny, smells ,bald";  //List of all joke categories
+var categories  = "fat, stupid, ugly, old, poor, short, skinny, smells, bald";  //List of all joke categories
 
 /**
- * MinecraftHelper is a child of AlexaSkill.
+ * JokeHelper is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var MinecraftHelper = function () {
+var JokeHelper = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-MinecraftHelper.prototype = Object.create(AlexaSkill.prototype);
-MinecraftHelper.prototype.constructor = MinecraftHelper;
+JokeHelper.prototype = Object.create(AlexaSkill.prototype);
+JokeHelper.prototype.constructor = JokeHelper;
 
-MinecraftHelper.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    var speechText = "Welcome to Yo Mama Jokes by Henry Schaumburger. You can request a category by saying, ask Yo Mama about category name..." +  
-                     "Categories include... " + categories + "... Now, what category would you like?";
+JokeHelper.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    var speechText = "Welcome to Yo Mama Jokes by Henry Schaumburger. You can request a category by saying, ask Yo Mama about a category such as ..." +  
+                     categories + "... Now, what category would you like?";
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
     var repromptText = "For instructions on what you can say, please say help me.";
     response.ask(speechText, repromptText);
 };
 
-MinecraftHelper.prototype.intentHandlers = {
-    "RecipeIntent": function (intent, session, response) {
-        console.log("INTENT: " + intent);
-        console.log("SESSION: " + session);
-        console.log("RESPONSE: " + response);
+JokeHelper.prototype.intentHandlers = {
+    "JokeIntent": function (intent, session, response) {
         var itemSlot = intent.slots.Item,
-            itemName;
+            categoryName;
         if (itemSlot && itemSlot.value){
-            itemName = itemSlot.value.toLowerCase();
+            categoryName = itemSlot.value.toLowerCase();
         }
 
-        var cardTitle = "Joke for category: " + itemName,
+        var cardTitle = "Joke for category: " + categoryName,
             jokeIndex,
-            recipe,
+            joke,
             speechOutput,
             repromptOutput;
         
+        //Select a randon category if one is not provided
+        if (categoryName === undefined) {
+            var categoryArray = categories.split(', ');
+            var categoryIndex = getRandomInt(0, categoryArray.length - 1);
+            categoryName = categoryArray[categoryIndex];
+        } 
+        
         //Lookup joke only when category is valid
-        if (isValidCategory(itemName)) {
-            jokeIndex = getRandomInt(0, recipes[itemName].length - 1),
-            recipe = recipes[itemName][jokeIndex];
+        if (isValidCategory(categoryName)) {
+            jokeIndex = getRandomInt(0, jokes[categoryName].length - 1),
+            joke = jokes[categoryName][jokeIndex];
         }        
-        if (recipe) {
+        if (joke) {
             speechOutput = {
-                speech: recipe,
+                speech: joke,
                 type: AlexaSkill.speechOutputType.PLAIN_TEXT
             };
-            response.tellWithCard(speechOutput, cardTitle, recipe);
+            response.tellWithCard(speechOutput, cardTitle, joke);
         } else {
             var speech;
-            if (itemName) {
-                speech = "I'm sorry, I currently do not know jokes in the category of " + itemName + 
-                         ". Now, what category would you like?  Choices include " + categories;
+            if (categoryName) {
+                speech = "I'm sorry, I currently do not know jokes in the " + categoryName + " category. " 
+                         "Now, what category would you like?  Choices include " + categories;
             } else {
-                speech = "I'm sorry, I currently do not know that category." + 
+                speech = "I'm sorry, I currently do not know that category. " + 
                          "What category would you like?  Choices include " + categories;
             }
             speechOutput = {
@@ -101,12 +105,12 @@ MinecraftHelper.prototype.intentHandlers = {
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
-        var speechOutput = "Goodbye from stop";
+        var speechOutput = "Goodbye";
         response.tell(speechOutput);
     },
 
     "AMAZON.CancelIntent": function (intent, session, response) {
-        var speechOutput = "Goodbye from cancel";
+        var speechOutput = "Goodbye";
         response.tell(speechOutput);
     },
 
@@ -128,8 +132,8 @@ MinecraftHelper.prototype.intentHandlers = {
 };
 
 exports.handler = function (event, context) {
-    var minecraftHelper = new MinecraftHelper();
-    minecraftHelper.execute(event, context);
+    var jokeHelper = new JokeHelper();
+    jokeHelper.execute(event, context);
 };
 
 /**
